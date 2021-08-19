dfffd7dfff0a053f5a58a3e54282cf87b99b0a3a
f4lconwings <nrs1411@outlook.de>
2021-08-19 23:03:10 +0200

    dependency and configuration setup

---
+++ shell/altv-update-win.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" = "release" ] || [ "$1" = "rc" ] || [ "$1" = "dev" ]; then 
+    echo "Downloading newest updates"
+else
+    echo "Invalid parameter, try 'release', 'rc' or 'dev'"
+    exit 1
+fi
+
+cd ../bin
+
+curl -o server.exe https://cdn.altv.mp/server/$1/x64_win32/altv-server.exe
+    echo "server loaded"
+
+mkdir -p modules/js-module && cd $_
+curl --remote-name-all https://cdn.altv.mp/js-module/$1/x64_win32/modules/js-module/{js-module,libnode}.dll
+    echo "js module loaded"
+cd ../..
+
+mkdir -p data && cd $_
+curl --remote-name-all https://cdn.altv.mp/server/$1/x64_win32/data/{vehmodels,vehmods}.bin
+    echo "vehicle binaries loaded"
+cd ..
+
+$SHELL
