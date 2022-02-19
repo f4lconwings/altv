563583e0f7ef76e8c1089ed468e79c8fc80bec7b
Nick Schneider <nrs1411@outlook.de>
2022-02-19 11:50:58 +0100

    Init interface

---
+++ src/interface/script/components/ServerIcon.tsx
@@ -0,0 +1,9 @@
+import icon from "/server_icon.png";
+
+export default function ServerIcon() {
+  return (
+    <div className="cServerIcon">
+      <img src={icon}></img>
+    </div>
+  );
+}
--- src/interface/script/pages/Loader.tsx
+++ src/interface/script/pages/Loader.tsx
@@ -1,7 +1,53 @@
 // import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import ServerIcon from "../components/ServerIcon";
+
+type DisplayText = { top: string; bottom?: string };
+type LoaderState = {
+  progress: number;
+  text: DisplayText;
+};
 
 export default function Loader() {
+  const [state, setState] = useState<LoaderState>({
+    progress: 0,
+    text: { top: "Initializing" },
+  });
+
+  useEffect(() => {
+    if ("alt" in window === false) return;
+
+    alt.on("app:req:loader", setReadiness);
+    function setReadiness() {
+      alt.emit("app:res:loader");
+      alt.off("app:req:loader", setReadiness);
+    }
+
+    const receivePayload = (payload: LoaderState) => setState(payload);
+    alt.on("loader:payload", receivePayload);
+
+    return function cleanup() {
+      alt.off("loader:payload", receivePayload);
+    };
+  });
   // const navigate = useNavigate();
 
-  return <></>;
+  return (
+    <div className="rLoader">
+      <div className="_wrapper">
+        <div className="_icon">
+          <ServerIcon />
+        </div>
+        <div className="_displayArea">
+          <div className="_progressBarWrapper">
+            <div style={{ width: `${state.progress}%` }} className="_progress"></div>
+          </div>
+          <div className="_text">
+            <div className="_topText">{state.text.top || "..."}</div>
+            <div className="_bottomText">{state.text.bottom || "..."}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
